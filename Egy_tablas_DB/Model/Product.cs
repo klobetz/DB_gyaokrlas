df885f022f07725bfd606881f41d8322b4351bd4
klobetz <klobetz@gmail.com>
2024-04-17 17:53:21 +0200

    a model létrehozása

---
+++ Egy_tablas_DB/Model/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Egy_tablas_DB.Model
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Peice { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        public DateTime LastModifiedAt { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public bool ShowData { get; set; } = true;
+
+        public bool GetData()
+        {
+            return ShowData;
+        }
+    }
+}
